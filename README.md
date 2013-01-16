92c4d392fa2b2a05920798c66f8bf4097bf52d2c
Vlastimil Holer <holer@ics.muni.cz>
2013-01-16 18:22:20 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,14 @@
+# Puppet yum module
+
+...
+
+### Requirements
+
+Module has been tested on:
+
+* Puppet 3.0
+* CentOS 6.3
+
+# Quick Start
+
+...
---
+++ tests/rpm_gpgkey.pp
@@ -0,0 +1,5 @@
+yum::rpm_gpgkey {
+	'/etc/pki/rpm-gpg/RPM-GPG-KEY-puppet-smoketest1':
+		ensure	=> absent,
+		content	=> '-----BEGIN PGP PUBLIC KEY BLOCK-----...',
+}

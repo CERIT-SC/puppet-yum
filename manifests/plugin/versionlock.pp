7446be50e9710761e966aea7ffdb42f6166955d8
Vlastimil Holer <holer@ics.muni.cz>
2013-02-14 18:22:52 +0100

    Add documentation to yum::plugin::versionlock

--- manifests/plugin/versionlock.pp
+++ manifests/plugin/versionlock.pp
@@ -1,3 +1,17 @@
+# Class: yum::plugin::versionlock
+#
+# This class installs versionlock plugin
+#
+# Parameters:
+#   [*ensure*] - specifies if versionlock should be present or absent
+#
+# Actions:
+#
+# Requires:
+#
+# Sample usage:
+#   include yum::plugin::versionlock
+#
 class yum::plugin::versionlock (
   $ensure = present
 ) {

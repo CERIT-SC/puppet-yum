1de71f16bb39863c75dff95c56e0714c5ec7efe3
Jon Skarpeteig <jskarpet@cisco.com>
2016-09-26 10:41:10 +0200

    Adding baseline acceptance tests

---
+++ spec/acceptance/class_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper_acceptance'
+
+describe 'yum class' do
+  context 'default parameters' do
+    # Using puppet_apply as a helper
+    it 'should work idempotently with no errors' do
+      pp = <<-EOS
+      class { 'yum': }
+      EOS
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes  => true)
+    end
+
+  end
+end

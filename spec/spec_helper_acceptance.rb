30c89ce3cd2eb788bbcac7b49145bd23c39b58e8
Dennis Hoppe <github@debian-solutions.de>
2016-10-22 11:28:32 +0200

    Fix requirements for Beaker tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,6 +1,7 @@
 require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
 
-install_puppet_agent_on hosts, {} unless ENV['BEAKER_provision'] == 'no'
+run_puppet_install_helper
 
 RSpec.configure do |c|
   # Project root
@@ -12,9 +13,11 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     # Install module and dependencies
-    puppet_module_install(source: proj_root, module_name: 'yum')
     hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
+      copy_module_to(host, source: proj_root, module_name: 'yum')
+      shell("/bin/touch #{default['puppetpath']}/hiera.yaml")
+      on host, puppet('module install puppetlabs-concat'), acceptable_exit_codes: [0, 1]
+      on host, puppet('module install puppetlabs-stdlib'), acceptable_exit_codes: [0, 1]
     end
   end
 end

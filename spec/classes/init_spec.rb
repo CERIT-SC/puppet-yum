b261b8d9f58ef05f7a7565e93abea94f37338319
Lucas Yamanishi <lucas.yamanishi@onyxpoint.com>
2017-01-20 15:08:38 -0500

    Cleanup init spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,35 +7,24 @@ describe 'yum' do
         facts
       end
 
-      context 'yum class without any parameters' do
-        it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
 
-        it do
-          is_expected.to contain_yum__config('installonly_limit').with(
-            ensure: '5',
-            notify: 'Exec[package-cleanup_oldkernels]'
-          )
-        end
+      context 'without any parameters' do
+        let(:params) { {} }
+
+        it { is_expected.to contain_yum__config('installonly_limit').with_ensure('5').that_notifies('Exec[package-cleanup_oldkernels]') }
       end
 
-      context 'clean_old_kernels => false' do
+      context 'when clean_old_kernels => false' do
         let(:params) { { clean_old_kernels: false } }
+
         it { is_expected.to contain_yum__config('installonly_limit').without_notify }
       end
     end
   end
-end
 
-context 'unsupported operating system' do
-  describe 'yum class without any parameters on Solaris/Nexenta' do
-    let(:facts) do
-      {
-        os: {
-          family: 'Solaris',
-          name:   'Nexenta'
-        }
-      }
-    end
+  context 'on an unsupported operating system' do
+    let(:facts) { { os: { family: 'Solaris', name: 'Nexenta' } } }
 
     it { is_expected.to raise_error(Puppet::Error, %r{Nexenta not supported}) }
   end

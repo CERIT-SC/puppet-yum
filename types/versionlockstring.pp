c547ecfae31ffe48919831f08fc8c16761bcda9d
Steve Traylen <steve.traylen@cern.ch>
2017-05-29 13:30:49 +0200

    Versionlock release string may contain dots
    Case example:
    
    ```
    1:java-1.7.0-openjdk-1.7.0.121-2.6.8.0.el7_3.x86_64
    ```
    
    Is a valid `E:N-V-R` for yum versionlock.
    In this case the release is as defined
    Yum::VersionlockString is `2.6.8.0`  and so
    dots must be permitted.

--- types/versionlockstring.pp
+++ types/versionlockstring.pp
@@ -12,8 +12,8 @@
 #   # VERSION: Any valid version string. The only limitation here, according to the RPM manual, is that it may not contain a dash (`-`).
 #   type Yum::PackageVersion = Regexp[/[^-]+/]
 #
-#   # RELEASE: An unsigned integer
-#   type Yum::PackageRelease = Regexp[/[0-9\*]+/]
+#   # RELEASE: Any valid release string. Only limitation is that it is not a dash (`-`)
+#   type Yum::PackageRelease = Regexp[/[^-]+/]
 #
 #   # ARCH: Matches a string such as `el7.x86_64`.  This is actuall two sub-expressions.  See below.
 #   type Yum::PackageArch    = Regexp[/([0-9a-zZ-Z_\*]+)(?:\.(noarch|x86_64|i386|arm|ppc64|ppc64le|sparc64|ia64|alpha|ip|m68k|mips|mipsel|mk68k|mint|ppc|rs6000|s390|s390x|sh|sparc|xtensa|\*))?/]
@@ -40,4 +40,4 @@
 # @example An invalid string (wildcard spans the VERSION and RELEASE fields): `0:bash-4.*-el6.x86_64
 # @example An invlaid string (wildcard spans the VERSION, RELEASE, and ARCH fields): `0:bash-*`
 #
-type Yum::VersionlockString = Pattern[/^([0-9\*]+):([0-9a-zA-Z\._\+%\{\}\*-]+)-([^-]+)-([0-9\*]+)\.(([0-9a-zZ-Z_\*]+)(?:\.(noarch|x86_64|i386|arm|ppc64|ppc64le|sparc64|ia64|alpha|ip|m68k|mips|mipsel|mk68k|mint|ppc|rs6000|s390|s390x|sh|sparc|xtensa|\*))?)$/]
+type Yum::VersionlockString = Pattern[/^([0-9\*]+):([0-9a-zA-Z\._\+%\{\}\*-]+)-([^-]+)-([^-]+)\.(([0-9a-zZ-Z_\*]+)(?:\.(noarch|x86_64|i386|arm|ppc64|ppc64le|sparc64|ia64|alpha|ip|m68k|mips|mipsel|mk68k|mint|ppc|rs6000|s390|s390x|sh|sparc|xtensa|\*))?)$/]

310783e5c05147d1520e7d338348e42e10916a2d
Keith Ward <keith.ward@lmax.com>
2020-01-23 15:48:32 +0000

    Restrict when to Just Fedora, everything else should be yum-utils

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -363,13 +363,13 @@ describe 'yum' do
 
       context 'when utils_package_name is not set' do
         case facts[:os]['name']
-        when 'Fedora', 'RedHat', 'CentOS', 'Scientific', 'Amazon', 'OEL', 'OracleLinux', 'VirtuozzoLinux'
+        when 'Fedora'
           case facts[:os]['release']['major']
           when '27', '28', '29', '30', '31', '32'
             it { is_expected.to contain_package('dnf-utils') }
-          else
-            it { is_expected.to contain_package('yum-utils') }
           end
+        else
+          it { is_expected.to contain_package('yum-utils') }
         end
       end
       context 'when utils_package_name is set' do

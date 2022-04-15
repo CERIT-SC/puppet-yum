800c5906cf96950d4302797adf3671ca49fe5ac8
Pat Riehecky <riehecky@fnal.gov>
2022-04-15 16:25:02 -0500

    Finish out dnf path for versionlock.

--- manifests/plugin/versionlock.pp
+++ manifests/plugin/versionlock.pp
@@ -3,42 +3,47 @@
 #
 # @param ensure specifies if versionlock should be present or absent
 # @param clean specifies if yum clean all should be called after edits. Defaults false.
-# @param path filepath for the versionlocks
+# @param path filepath for the versionlock.list, default based on your system.
 #
 # @example Sample usage:
-#   include yum::plugin::versionlock
+#   class { 'yum::plugin::versionlock':
+#     ensure      => present,
+#   }
 #
 class yum::plugin::versionlock (
-  Enum['present', 'absent'] $ensure = 'present',
-  String                    $path   = '/etc/yum/pluginconf.d/versionlock.list',
-  Boolean                   $clean  = false,
+  String                    $path,
+  Enum['present', 'absent'] $ensure   = 'present',
+  Boolean                   $clean    = false,
 ) {
-  $pkg_prefix = $facts['os']['release']['major'] ? {
-    Variant[Integer[5,5], Enum['5']] => 'yum',
-    '8' => 'python3-dnf-plugin',
-    default => 'yum-plugin',
+  $pkg_prefix = $facts['package_provider'] ? {
+    'dnf'   => 'python3-dnf-plugin',
+    'yum'   => 'yum-plugin',
+    default => '',
   }
+
   yum::plugin { 'versionlock':
     ensure     => $ensure,
     pkg_prefix => $pkg_prefix,
   }
 
-  include yum::clean
-  $_clean_notify = $clean ? {
-    true  => Exec['yum_clean_all'],
-    false => undef,
-  }
+  if $ensure == 'present' {
+    include yum::clean
+    $_clean_notify = $clean ? {
+      true  => Exec['yum_clean_all'],
+      false => undef,
+    }
 
-  concat { $path:
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
-    notify => $_clean_notify,
-  }
+    concat { $path:
+      mode   => '0644',
+      owner  => 'root',
+      group  => 'root',
+      notify => $_clean_notify,
+    }
 
-  concat::fragment { 'versionlock_header':
-    target  => $path,
-    content => "# File managed by puppet\n",
-    order   => '01',
+    concat::fragment { 'versionlock_header':
+      target  => $path,
+      content => "# File managed by puppet\n",
+      order   => '01',
+    }
   }
 }
